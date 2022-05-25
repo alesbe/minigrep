3b4225500ab9416427c228557d0ccde47f4d7440
alesbe <aesparzabellver@gmail.com>
2022-05-26 00:06:59 +0200

    Moved logic to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+use std::fs::File;
+use std::io::Read;
+
+pub struct Config {
+    pub query: String,
+    pub file: String
+}
+
+impl Config {
+    pub fn new (args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Missing arguments");
+        }
+
+        let file = &args[1].clone();
+        let query = &args[2].clone();
+
+        Ok(Config { query: query.to_string() , file: file.to_string() })
+    }
+}
+
+pub fn run (config: Config) -> Result<(), &'static str> {
+    // Open file
+    let mut file = match File::open(config.file) {
+        Ok(f) => f,
+        Err(_) => return Err("File not found")
+    };
+
+    // Dump file contents
+    let mut data = String::new();
+    match file.read_to_string(&mut data) {
+        Ok(s) => s,
+        Err(_) => return Err("Cannot open file")
+    };
+
+    // Search data
+    let data = search(&data, &config.query);
+
+    // Print data
+    if data.len() < 1 { println!("[No results containing {}]", config.query) };
+    
+    for line in data {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a> ( data: &'a String, query: &'a String ) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in data.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process;
-use std::fs::File;
-use std::io::Read;
+
+use minigrep::Config;
 
 fn main() {
     // Take args
@@ -20,65 +20,8 @@ fn main() {
     };
 
     // Run minigrep 
-    match run(config) {
+    match minigrep::run(config) {
         Ok(_) => (),
         Err(e) => println!("Error: {}", e)
     };
-}
-
-struct Config {
-    query: String,
-    file: String
-}
-
-impl Config {
-    fn new (args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Missing arguments");
-        }
-
-        let file = &args[1].clone();
-        let query = &args[2].clone();
-
-        Ok(Config { query: query.to_string() , file: file.to_string() })
-    }
-}
-
-fn run (config: Config) -> Result<(), &'static str> {
-    // Open file
-    let mut file = match File::open(config.file) {
-        Ok(f) => f,
-        Err(_) => return Err("File not found")
-    };
-
-    // Dump file contents
-    let mut data = String::new();
-    match file.read_to_string(&mut data) {
-        Ok(s) => s,
-        Err(_) => return Err("Cannot open file")
-    };
-
-    // Search data
-    let data = search(&data, &config.query);
-
-    // Print data
-    if data.len() < 1 { println!("[No results containing {}]", config.query) };
-    
-    for line in data {
-        println!("{}", line);
-    }
-
-    Ok(())
-}
-
-fn search<'a> ( data: &'a String, query: &'a String ) -> Vec<&'a str> {
-    let mut result = Vec::new();
-
-    for line in data.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-
-    result
 }
