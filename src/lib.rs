1200f2aedc05709add7668e38d6c6469db8bb88a
alesbe <aesparzabellver@gmail.com>
2022-05-26 00:09:56 +0200

    Improved comments

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ impl Config {
         let file = &args[1].clone();
         let query = &args[2].clone();
 
+        // Create new config
         Ok(Config { query: query.to_string() , file: file.to_string() })
     }
 }
@@ -47,13 +48,16 @@ pub fn run (config: Config) -> Result<(), &'static str> {
 }
 
 pub fn search<'a> ( data: &'a String, query: &'a String ) -> Vec<&'a str> {
+    // Vector to append lines containing query
     let mut result = Vec::new();
 
+    // Iterate lines in data, if line contains query, push to vector
     for line in data.lines() {
         if line.contains(query) {
             result.push(line);
         }
     }
 
+    // Return vector of lines matching query
     result
 }
